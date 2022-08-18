548c07becf8e7428f1895e4e0495a86d1161e352
hensell de la rosa <henseldelarosa@DESKTOP-DOB1QI3.localdomain>
2022-08-18 03:49:12 -0400

    sign up

--- backend/routes/api/users.js
+++ backend/routes/api/users.js
@@ -1,4 +1,21 @@
 const express = require('express')
 const router = express.Router();
 
+const { setTokenCookie, requireAuth } = require('../../utils/auth');
+const { User } = require('../../db/models');
+
+router.post(
+  '/',
+  async (req, res) => {
+    const { firstName, lastName,email, password, username } = req.body;
+    console.log(password)
+    const user = await User.signup({ firstName, lastName,email, username, password });
+
+    await setTokenCookie(res, user);
+
+    return res.json({
+      user
+    });
+  }
+);
 module.exports = router;
