1dbb28145fb762baa93fa2a611537a41285d8a3a
hensell de la rosa <henseldelarosa@DESKTOP-DOB1QI3.localdomain>
2022-08-29 06:45:19 -0400

    dasd

--- backend/db/migrations/20220827185736-bookings.js
+++ backend/db/migrations/20220827185736-bookings.js
@@ -20,7 +20,7 @@ module.exports = {
         allowNull:false,
       },
       endDate: {
-        type: Sequelize.INTEGER,
+        type: Sequelize.DATE,
         allowNull:false,
       },
       createdAt: {
