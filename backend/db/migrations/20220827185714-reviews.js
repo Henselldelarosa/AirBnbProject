45d61fab5d4bfd2e91a220e99cd22eb91fb4373a
hensell de la rosa <henseldelarosa@DESKTOP-DOB1QI3.localdomain>
2022-08-29 07:29:28 -0400

    ada

--- backend/db/migrations/20220827185714-reviews.js
+++ backend/db/migrations/20220827185714-reviews.js
@@ -30,7 +30,7 @@ module.exports = {
         allowNull:false
       },
       stars: {
-        type: Sequelize.INTEGER,
+        type: Sequelize.REAL,
         allowNull:false
       },
       createdAt: {
