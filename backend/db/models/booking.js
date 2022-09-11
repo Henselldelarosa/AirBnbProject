e526a7bc95f8ae74f7bd25e54c7384d1dbcfba9d
hensell de la rosa <henseldelarosa@DESKTOP-DOB1QI3.localdomain>
2022-09-11 02:01:29 -0400

    fsf

--- backend/db/models/booking.js
+++ backend/db/models/booking.js
@@ -27,11 +27,11 @@ module.exports = (sequelize, DataTypes) => {
       onDelete: 'CASCADE'
     },
     startDate:{
-      type:DataTypes.DATEONLY,
+      type:DataTypes.DATE,
       allowNull:false,
     },
     endDate:{
-      type: DataTypes.DATEONLY,
+      type: DataTypes.DATE,
       allowNull:false
     },
   }, {
