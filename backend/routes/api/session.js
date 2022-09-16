76ec5f4a2875b6e3a99ce761a2bdf85f69ff5897
hensell de la rosa <henseldelarosa@DESKTOP-DOB1QI3.localdomain>
2022-09-16 08:00:53 -0400

    ssfsf

--- backend/routes/api/session.js
+++ backend/routes/api/session.js
@@ -8,7 +8,7 @@ const { check } = require('express-validator');
 const { handleValidationErrors } = require('../../utils/validation');
 
 
-
+//a
 // const flatten = (obj = {}, res = {}, extraKey = '') => {
 //   for(key in obj){
 //      if(typeof obj[key] !== 'object'){
