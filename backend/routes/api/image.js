973e3e7110943ac01394010e234690de283960e9
hensell de la rosa <henseldelarosa@DESKTOP-DOB1QI3.localdomain>
2022-09-17 19:17:46 -0400

    Dadaff

--- backend/routes/api/image.js
+++ backend/routes/api/image.js
@@ -2,7 +2,7 @@ const express = require('express');
 const { check } = require('express-validator');
 const { setTokenCookie, requireAuth, restoreUser } = require('../../utils/auth');
 const { handleValidationErrors, validateSignup } = require('../../utils/validation');
-const { User,Image } = require('../../db/models');
+const { User,Image,Spot,Review } = require('../../db/models');
 const router = express.Router();
 
 router.get('/', requireAuth, async(req,res,next)=>{
@@ -31,16 +31,34 @@ const user = await User.findOne({
     }
   })
 
-  if(!image){
-    const error = Error("Image couldn't be found");
-  error.status = 404;
-  return next(error);
-  }
-image.destroy()
+  let spot, review;
+  if(image){
+      spot = await Spot.findOne({
+      where:{
+        id:image.imageableId,
+        onwerId:user.id
+
+      }
+    })
+
+     review = await Review.findOne({
+      where:{
+        id:imageId.imageableId,
+        userId: user.id
+      }
+    })
+    image.destroy()
   res.json({
     message: "Successfully deleted",
     statusCode:200
   });
+
+  }
+if(!(spot || review)){
+  const error = new Error("Image couldn't be found")
+  error.status = 404
+  return next(error)
+}
 })
 
 module.exports = router;
