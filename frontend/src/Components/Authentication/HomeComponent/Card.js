e18497a61ccd3638eb214a81bd17752349f6ad47
hensell de la rosa <henseldelarosa@DESKTOP-DOB1QI3.localdomain>
2022-12-09 04:50:06 -0500

    fsfs

--- frontend/src/Components/Authentication/HomeComponent/Card.js
+++ frontend/src/Components/Authentication/HomeComponent/Card.js
@@ -1,17 +1,28 @@
 import React from 'react'
-import './Card.css'
+import { NavLink } from 'react-router-dom'
+ import './Card.css'
 
-function Card({src, title, description, price}) {
+function Card({src,place,rating,description,price}) {
+  const ratingCheck = (rating) =>{
+    if(rating === null){
+      return rating = 'new'
+    }else{
+      return rating
+    }
+  }
   return (
-    <div className='card'>
-      <img className='home_spot_image' src={src} alt=''/>
+    <div className='card_container'>
+      <img className='card_image'src={src}/>
       <div className='card_info'>
-        <h2>{title}</h2>
-        <h4>{description}</h4>
-        <h3>{price}</h3>
+        <h3>{place}</h3>
+        <div className='card_rating'>
+        <i class="fa-sharp fa-solid fa-star"/>
+        <p>{ratingCheck(rating)}</p>
+         </div>
       </div>
+      <h5><small>{description}</small></h5>
+         <div className='card_price'>${price} night</div>
     </div>
   )
 }
-
 export default Card
